1e2186447f2166c4e553627782d2d3de5a36d6a0
herorugby <hero.rugby1015@gmail.com>
2022-04-25 07:24:34 +0900

    new codes

---
+++ js/table.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#add').click(function () {
+        var maker = $("#maker").val();
+        var products = $("#products").val();
+        var sizecolor = $("#sizecolor").val();
+        var quantity = $("#quantity").val();
+        var pricetaxex = $("#pricetaxex").val();
+        $('')
+    });
+})
