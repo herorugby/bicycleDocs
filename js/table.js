ef90a66d5fa825ec4969389b6af29b79f8a6b820
herorugby <hero.rugby1015@gmail.com>
2022-04-26 06:29:45 +0900

    new codes

--- js/table.js
+++ js/table.js
@@ -1,10 +1,17 @@
-$(function () {
-    $('#add').click(function () {
-        var maker = $("#maker").val();
-        var products = $("#products").val();
-        var sizecolor = $("#sizecolor").val();
-        var quantity = $("#quantity").val();
-        var pricetaxex = $("#pricetaxex").val();
-        $('')
-    });
-})
+// $(function () {
+//     $('#add').click(function () {
+//         var maker = $("#maker").val();
+//         var products = $("#products").val();
+//         var sizecolor = $("#sizecolor").val();
+//         var quantity = $("#quantity").val();
+//         var pricetaxex = $("#pricetaxex").val();
+//         $('')
+//     });
+// })
+
+$(function() {
+ $("#addBtn").on("click", function() {
+  // テーブルの最終行をクローンしてテーブルの最後に追加する
+  $(".new-bicycle .addForm tr:last-child").clone(true).appendTo(".new-bicycle tbody");
+ });
+});
