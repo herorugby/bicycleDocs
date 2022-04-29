465474a991c379569cea99d30503819f2902c042
herorugby <hero.rugby1015@gmail.com>
2022-04-29 09:43:44 +0900

    new codes

--- js/table.js
+++ js/table.js
@@ -9,14 +9,25 @@
 //     });
 // })
 
-$(function() {
-    $("#addBtn").on("click", function () {
+$(function () {
+
+    // 新車情報フォームの行追加
+    $("#addBtnNewBike").on("click", function () {
         $(".new-bicycle tbody tr:first-child") // テーブルの一番初めの行を指定する
             .clone(true)                     // 指定した一番初めの行のHTML要素を複製する
             .appendTo(".new-bicycle tbody");   // 複製した要素をtbodyに追加する
 
-
         $(".new-bicycle tbody tr:last-child input").val(""); // 追加した行の値をクリアする
 
- });
+    });
+
+    // 修理自転車のフォーム行追加
+    $("#addBtnFixBike").on("click", function () {
+        $(".fix-bicycle tbody tr:first-child") // テーブルの一番初めの行を指定する
+            .clone(true)                     // 指定した一番初めの行のHTML要素を複製する
+            .appendTo(".fix-bicycle tbody");   // 複製した要素をtbodyに追加する
+
+        $(".fix-bicycle tbody tr:last-child input").val(""); // 追加した行の値をクリアする
+
+    });
 });
